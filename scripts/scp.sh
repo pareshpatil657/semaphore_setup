b002ece8d86e4aa0d15d3658eca7b0cad3d2988d
semaphore <paresh.patil@trustingsocial.com>
2018-10-17 18:49:27 +0530

    add files

--- scripts/scp.sh
+++ scripts/scp.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-scp -r /home/paresh/home/paresh/Repos/semaphore_setup/ semaphore_centos:/tmp/repositories/
-ssh semaphore_centos 'cp -r /tmp/repositories/.git  /tmp/repositories/semaphore_setup'
-ssh semaphore_centos 'python /tmp/repositories/devops_idea_semaphore/semaphore_api_call/main.py --username admin --password password --host http://0.0.0.0:3000 --project-file /tmp/repositories/devops_idea_semaphore/semaphore_config/import_config.json'
+ssh  semaphore_centos 'mkdir -p /tmp/semaphore_setup'
+scp -r /home/paresh/Repos/tsocial/semaphore_setup/ semaphore_centos:/tmp/semaphore_setup/
+ssh semaphore_centos 'cp -r /tmp/semaphore_setup/semaphore_setup/.git  /tmp/semaphore_setup/semaphore_setup.git'
+ssh semaphore_centos 'python /tmp/semaphore_setup/semaphore_setup/semaphore_api_call/main.py --username admin --password password --host http://0.0.0.0:3000 --project-file /tmp/semaphore_setup/semaphore_setup/semaphore_config/import_config.json'
 
 
 
